7f446d6b08d557bc265ba62e0027b7b9c820c369
Rupa Pyaram <71918720+rupapyaram@users.noreply.github.com>
2026-06-19 13:09:26 +0100

    Refactor LoginTest to extend BaseTest and use listeners

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -1,32 +1,22 @@
 package tests;
+
+import base.BaseTest;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-
 import Pages.LoginPage;
+import org.testng.annotations.Listeners;
+import listeners.TestListener;
 
-public class LoginTest {
-
-    WebDriver driver;
-
-    @BeforeMethod
-    public void setup() {
-        driver = new ChromeDriver();
-        driver.get("https://www.saucedemo.com/");
-    }
+@Listeners(TestListener.class)
+public class LoginTest extends BaseTest {
 
     @Test
     public void validLoginTest() {
 
-    	LoginPage loginPage = new LoginPage(driver);
+        LoginPage loginPage = new LoginPage(driver);
 
-    	loginPage.login(
-    	        "standard_user",
-    	        "secret_sauce");
+        loginPage.login("standard_user", "secret_sauce");
 
         Assert.assertEquals(driver.getTitle(), "Swag Labs");
     }
@@ -34,36 +24,26 @@ public class LoginTest {
     @Test
     public void invalidLoginTest() {
 
-    	LoginPage loginPage = new LoginPage(driver);
+        LoginPage loginPage = new LoginPage(driver);
 
-    	loginPage.login(
-    	        "wrong_user",
-    	        "wrong_password");
+        loginPage.login("wrong_user", "wrong_password");
 
         String errorMessage =
                 driver.findElement(By.cssSelector("h3[data-test='error']")).getText();
 
-        Assert.assertTrue(
-                errorMessage.contains("Username and password do not match"));
+        Assert.assertTrue(errorMessage.contains("Username and password do not match"));
     }
-    
+
     @Test
     public void lockedUserTest() {
 
         LoginPage loginPage = new LoginPage(driver);
 
-        loginPage.login(
-                "locked_out_user",
-                "secret_sauce");
+        loginPage.login("locked_out_user", "secret_sauce");
 
         String errorMessage =
                 driver.findElement(By.cssSelector("h3[data-test='error']")).getText();
 
-        Assert.assertTrue(
-                errorMessage.contains("Sorry, this user has been locked out."));
-    }
-    @AfterMethod
-    public void tearDown() {
-        driver.quit();
+        Assert.assertTrue(errorMessage.contains("Sorry, this user has been locked out."));
     }
 }
